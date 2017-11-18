e82882c1179282c58ee10d497545dc1068459e87
David Thompson <david@davidthompson.us>
2017-11-18 17:44:24 +0000

    renamed template

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'DashboardController@show');
+Route::resource('players', 'PlayerController');
+Route::resource('games', 'GameController');
