92fe6114149a808c3ce7b599e25513cd8a9839b1
David Thompson <david@davidthompson.us>
2017-11-19 21:10:11 +0000

    added seeder for test data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Player;
+use App\Game;
+use App\GamePlayer;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        for($i=0; $i < 500; $i++) {
+			factory(Player::class)->create();
+        }
+
+        for($i=0; $i < 3000; $i++) {
+        	$player1 = Player::inRandomOrder()->first();
+        	$player2 = Player::inRandomOrder()->where('id', '!=', $player1->id)->first();
+
+        	// generate two different scores
+        	$score1 = rand(0,50);
+        	do {
+        		$score2 = rand(0,50);
+        	} while ($score1 == $score2);
+
+        	$game = Game::create();
+
+        	$gamePlayer = new GamePlayer;
+        	$gamePlayer->game_id = $game->id;
+        	$gamePlayer->player_id = $player1->id;
+        	$gamePlayer->score = $score1;
+			$gamePlayer->save();
+
+        	$gamePlayer = new GamePlayer;
+        	$gamePlayer->game_id = $game->id;
+        	$gamePlayer->player_id = $player2->id;
+        	$gamePlayer->score = $score2;
+			$gamePlayer->save();
+        }   
     }
 }
