04c0b01a86fb4692f074308dfd7a9d0cc84b1994
David Thompson <david@davidthompson.us>
2017-11-19 01:40:07 -0500

    made the SQL easier to view

--- database/migrations/2017_11_19_002601_create_player_rankings_view.php
+++ database/migrations/2017_11_19_002601_create_player_rankings_view.php
@@ -13,58 +13,64 @@ class CreatePlayerRankingsView extends Migration
      */
     public function up()
     {
-        DB::statement("CREATE VIEW player_rankings AS 
-    WITH ranked_games 
-     AS (SELECT game_id, 
-                player_id, 
-                Rank() 
-                  over ( 
-                    PARTITION BY game_id 
-                    ORDER BY score DESC) game_rank 
-         FROM   game_players) 
-
-
-select  player_id,
-win_count,
-loss_count,
-win_ratio,
-       Rank() 
-         over ( 
-           ORDER BY win_ratio DESC) 
-
-            from (
-
-SELECT 
-       player_id, 
-       win_count, 
-       loss_count, 
-
-       case when win_count = 0 then 0 else
-       win_count / (win_count + loss_count) :: float
-       end win_ratio
-FROM   (SELECT players.id              player_id, 
-               Coalesce(win_count, 0)  win_count, 
-               Coalesce(loss_count, 0) loss_count
-        FROM   players 
-               left join (SELECT player_id, 
-                                 Count(*) win_count 
-                          FROM   (SELECT game_id, 
-                                         player_id, 
-                                         game_rank 
-                                  FROM   ranked_games) game_wins 
-                          WHERE  game_rank = 1 
-                          GROUP  BY player_id) win_table 
-                      ON win_table.player_id = players.id 
-               left join (SELECT player_id, 
-                                 Count(*) loss_count 
-                          FROM   (SELECT game_id, 
-                                         player_id, 
-                                         game_rank 
-                                  FROM   ranked_games) game_wins 
-                          WHERE  game_rank != 1 
-                          GROUP  BY player_id) loss_table 
-                      ON loss_table.player_id = players.id)win_totals) win_ratios
-                      ");
+        $viewSql = <<<EOT
+CREATE VIEW player_rankings
+AS
+WITH ranked_games
+AS (SELECT
+  game_id,
+  player_id,
+  RANK()
+  OVER (
+  PARTITION BY game_id
+  ORDER BY score DESC) game_rank
+FROM game_players)
+SELECT
+  player_id,
+  win_count,
+  loss_count,
+  win_ratio,
+  RANK()
+  OVER (
+  ORDER BY win_ratio DESC)
+FROM (SELECT
+  player_id,
+  win_count,
+  loss_count,
+  CASE
+    WHEN win_count = 0 THEN 0
+    ELSE win_count / (win_count + loss_count) ::float
+  END win_ratio
+FROM (SELECT
+  players.id player_id,
+  COALESCE(win_count, 0) win_count,
+  COALESCE(loss_count, 0) loss_count
+FROM players
+LEFT JOIN (SELECT
+  player_id,
+  COUNT(*) win_count
+FROM (SELECT
+  game_id,
+  player_id,
+  game_rank
+FROM ranked_games) game_wins
+WHERE game_rank = 1
+GROUP BY player_id) win_table
+  ON win_table.player_id = players.id
+LEFT JOIN (SELECT
+  player_id,
+  COUNT(*) loss_count
+FROM (SELECT
+  game_id,
+  player_id,
+  game_rank
+FROM ranked_games) game_wins
+WHERE game_rank != 1
+GROUP BY player_id) loss_table
+  ON loss_table.player_id = players.id) win_totals) win_ratios
+EOT;        
+  
+        DB::statement($viewSql);
     }
 
     /**
