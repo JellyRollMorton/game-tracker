787d59eb4090e9ecc8191c403e3b8db69afdd2fe
David Thompson <david@davidthompson.us>
2017-11-19 19:29:18 -0500

    added a glyph logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="/">{{env('APP_NAME', 'My Application Name')}}</a>
+            <a class="navbar-brand" href="/"><span class="logo-icon glyphicon glyphicon-stats" aria-hidden="true"></span> {{env('APP_NAME', 'My Application Name')}}</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
